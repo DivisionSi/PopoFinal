d100dcd5ebb44a8ce3a2918d06361a988529f787
MusabAkram <musabakram16@gmail.com>
2016-12-09 22:05:31 +0500

    saad

--- app/src/main/java/com/gmail/saadbnwhd/popofinal/Saad.java
+++ app/src/main/java/com/gmail/saadbnwhd/popofinal/Saad.java
@@ -1,7 +1,14 @@
 package com.gmail.saadbnwhd.popofinal;
 
+import android.os.Bundle;
+
 /**
  * Created by Musab on 12/9/2016.
  */
 public class Saad {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
