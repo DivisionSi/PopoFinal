142ec02b58405a911dec046f9ef681b3eabc23b8
Saad Bin Waheed <saadbnwhd@gmail.com>
2016-12-09 21:05:59 +0500

    Add files via upload

---
+++ app/src/main/java/com/gmail/saadbnwhd/popofinal/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.example.musab.poponfa;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.musab.poponfa.R;
+
+/**
+ * Created by Musab on 12/6/2016.
+ */
+public class HomeFragment extends Fragment {
+    public HomeFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
